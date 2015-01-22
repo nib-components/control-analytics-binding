a7a9b70253aea8f96e4fdc759c22bba4a83a6176
James Newell <j.newell@nib.com.au>
2015-01-22 11:44:39 +1100

    got the ga data being pulled from radio/checkbox inputs too

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 var ga = require('analytics');
 
 /**
+ * Find the input element with the matching value
+ * @param   {HTMLElement} element
+ * @param   {string}      value
+ * @returns {HTMLElement}
+ */
+function findElement(element, value) {
+
+  //find the inputs on the element
+  var inputs = element.querySelectorAll('input,textarea,select');
+  for (var i=0; i<inputs.length; ++i) {
+    if (inputs[i].value === value) {
+      return inputs[i];
+    }
+  }
+
+  //fallback to the input element
+  return element;
+}
+
+/**
  * A control plugin - binds analytics to the control validate event
  * @param   options
  * @returns {function}
@@ -10,15 +30,22 @@ module.exports = function(options) {
 
   return function(control) {
 
-    var gaCategory  = options.category || control.inputView.el.getAttribute('data-analytics-category');
-    var gaLabel     = options.label || control.inputView.el.getAttribute('data-analytics-label');
-    var gaIgnore    = control.inputView.el.getAttribute('data-analytics-ignore') !== null;
+    //track the event
+    control.on('validate', function(valid) {
 
-    if (gaIgnore) {
-      return;
-    }
+      //find the input element
+      var value = control.inputView.getValue(); //use the raw view value otherwise we won't find an element if we use the filtered control value
+      var element = findElement(control.inputView.el, value);
 
-    control.on('validate', function(valid, value) {
+      //get the event data
+      var gaCategory  = options.category || element.getAttribute('data-analytics-category') || '';
+      var gaLabel     = options.label || element.getAttribute('data-analytics-label') || '';
+      var gaIgnore    = element.getAttribute('data-analytics-ignore') !== null;
+
+      //ignore the event
+      if (gaIgnore) {
+        return;
+      }
 
       //replace template values
       gaCategory  = gaCategory.replace('{{value}}', value);
